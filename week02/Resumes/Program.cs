c5c852f314af7dd11b58c5640ff2128a473e056a
willieboat <willieboat2@gmail.com>
2025-05-17 23:26:10 +0000

    week 02 activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Aviation Profiler";
+        job1._company = "APSS";
+        job1._startYear = 2017;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Security Officer";
+        job2._company = "ACJCOLDS";
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "William Ntow Boateng";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
