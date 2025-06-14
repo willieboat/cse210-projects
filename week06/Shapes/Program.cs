f1b4ae05bf829d48aa7bb8ba53fa4026040eb517
willieboat <willieboat2@gmail.com>
2025-06-14 10:07:09 +0000

    week06

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        var shapes = new List<Shape>
+        {
+            new Square("Red", 5),
+            new Rectangle("Blue", 4, 6),
+            new Circle("Green", 3)
+        };
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea():F2}");
+            Console.WriteLine();
+        }
     }
 }
